524a4ec770ea3e8161c74dc3d3c743b037621b5e
Nadia <nadia.shevtsova@oceanshq.com>
2023-11-16 12:47:00 +1000

    Play 162. Adding Navigation to the MealDetails Screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,12 +5,15 @@ import 'package:transparent_image/transparent_image.dart';
 import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({
-    super.key,
-    required this.meal,
+  const MealItem(
+      {super.key,
+      required this.meal,
+      required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
+
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
@@ -32,7 +35,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge, // to have a shape visible
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
